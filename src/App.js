1eff2561f0b1349dd235e35c3497e1d1ec7e67bd
93wilsonlu <93wilsonlu@gmail.com>
2022-06-14 15:48:54 +0800

    Add url storage function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*global chrome*/
 import "./App.css";
 import React from "react";
 import { List } from "./components/List";
@@ -12,19 +13,34 @@ class App extends React.Component {
         this.addURL = this.addURL.bind(this);
         this.deleteURL = this.deleteURL.bind(this);
     }
-    addURL(url) {
+    async componentDidMount() {
+        const result = await chrome.storage.sync.get(["url_list"]);
+        this.setState({ url_list: result.url_list || [] });
+    }
+    async addURL(url) {
+        let new_url_list = [
+            ...this.state.url_list,
+            { id: +new Date(), url: url },
+        ];
         this.setState({
-            url_list: [...this.state.url_list, { id: +new Date(), url: url }],
+            url_list: new_url_list,
+        });
+        chrome.storage.sync.set({ url_list: new_url_list }, () => {
+            console.log("Added");
         });
     }
-    deleteURL(id) {
+    async deleteURL(id) {
+        let new_url_list = this.state.url_list.filter((item) => item.id !== id);
         this.setState({
-            url_list: this.state.url_list.filter((item) => item.id !== id),
+            url_list: new_url_list,
+        });
+        chrome.storage.sync.set({ url_list: new_url_list }, () => {
+            console.log("Deleted");
         });
     }
     render() {
         return (
-            <div className="container mt-1">
+            <div className="container mt-2">
                 <div className="row">
                     <h1>Auto History Filter</h1>
                 </div>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 class ListItem extends React.Component {
     render() {
         return (
-            <li className="list-group-item d-flex justify-content-between">
+            <li className="list-group-item d-flex justify-content-between py-1">
                 {this.props.item.url}
                 <button className="btn btn-sm" onClick={() => this.props.handleDelete(this.props.item.id)}>
                     <i className="bi bi-trash-fill"></i>
